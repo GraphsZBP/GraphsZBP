b06342c16134fa1fdf115151e0b1979113a6f97f
msulima@github.com <msulima@github.com>
2013-12-28 16:31:40 +0100

    Little refactoring

--- src/original/depth_first.cpp
+++ src/original/depth_first.cpp
@@ -1,32 +1,31 @@
 #include "depth_first.h"
 #include <iostream>
 
-void zwiedzaj(int **G, int *V, int i, int n);
+void depth_first_visit(int **G, int *V, int i, int n);
 
-void szukaj(int **G, int *V, int n) {
+void depth_first(int **G, int *V, int n) {
   int i;
   for (i = 0; i < n; i++) {
     V[i] = 0;
   }
   for (i = 0; i < n; i++) {
     if (V[i] == 0) {
-      zwiedzaj(G, V, i, n);
+      depth_first_visit(G, V, i, n);
     }
   }
 }
 
-void zwiedzaj(int **G, int *V, int i, int n) {
+void depth_first_visit(int **G, int *V, int i, int n) {
   std::cout << i << " ";
   V[i] = 1;
   for (int k = 0; k < n; k++) {
     if (G[i][k] != 0) {
       if (V[k] == 0) {
-        zwiedzaj(G, V, k, n);
+        depth_first_visit(G, V, k, n);
       }
     }
   }
 }
-
 
 void original_depth_first_sample() {
   const int n = 7;
@@ -39,5 +38,5 @@ void original_depth_first_sample() {
   G[4] = new int[n] { 1, 1, 0, 1, 1, 1, 0 };
   G[5] = new int[n] { 0, 0, 0, 0, 1, 1, 0 };
   G[6] = new int[n] { 0, 0, 1, 1, 0, 0, 1 };
-  szukaj(G, V, n);
+  depth_first(G, V, n);
 }
--- src/original/depth_first.h
+++ src/original/depth_first.h
@@ -2,6 +2,5 @@
 #define ORIGINAL_DEPTH_FIRST_H_
 
 void original_depth_first_sample();
-void szukaj(int *G[], int *V, int n);
 
 #endif /* ORIGINAL_DEPTH_FIRST_H_ */
