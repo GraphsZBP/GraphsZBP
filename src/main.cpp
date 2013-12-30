3dbaae71b6eab1286c220dd5688092c1f82f4726
msulima <sulima.mateusz@gmail.com>
2013-12-30 13:57:59 +0100

    Measuring time

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <stdlib.h>
@@ -9,15 +10,33 @@
 #include "boost/breadth_first.h"
 
 void test(const std::shared_ptr<graph_generator>& graph) {
+  using namespace std::chrono;
+
+  high_resolution_clock::time_point begin;
+  duration<double> time_span;
+
   std::cout << "Depth first" << std::endl;
+
+  begin = high_resolution_clock::now();
   original_depth_first_sample(graph);
-  std::cout << std::endl;
+  time_span = duration_cast<duration<double>>(high_resolution_clock::now() - begin);
+  std::cout << std::endl << time_span.count() << std::endl;
+
+  begin = high_resolution_clock::now();
   boost_depth_first_sample(graph);
+  time_span = duration_cast<duration<double>>(high_resolution_clock::now() - begin);
+  std::cout << std::endl << time_span.count() << std::endl;
+
   std::cout << std::endl << "Breadth first" << std::endl;
+  begin = high_resolution_clock::now();
   original_breadth_first_sample(graph);
-  std::cout << std::endl;
+  time_span = duration_cast<duration<double>>(high_resolution_clock::now() - begin);
+  std::cout << std::endl << time_span.count() << std::endl;
+
+  begin = high_resolution_clock::now();
   boost_breadth_first_sample(graph);
-  std::cout << std::endl;
+  time_span = duration_cast<duration<double>>(high_resolution_clock::now() - begin);
+  std::cout << std::endl << time_span.count() << std::endl;
 }
 
 int main() {
@@ -27,7 +46,7 @@ int main() {
   test(graph);
 
   std::cout << "Random" << std::endl;
-  graph_generator *random_generator = new graph_generator(10, 90);
+  graph_generator *random_generator = new graph_generator(5000, 90);
   std::shared_ptr<graph_generator> random_graph(random_generator);
   //random_generator->print();
   test(random_graph);
