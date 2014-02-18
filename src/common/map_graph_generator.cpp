d2337ff93fc0bf4d69b059fcd650351bd722000f
msulima <sulima.mateusz@gmail.com>
2014-02-18 14:48:41 +0100

    Restored demo methods

--- src/common/map_graph_generator.cpp
+++ src/common/map_graph_generator.cpp
@@ -11,12 +11,12 @@ map_graph_generator::map_graph_generator(size_t size, int fill, unsigned int nei
 
 zbp::distance_matrix map_graph_generator::original_graph() {
   if (m_original_graph == NULL) {
-    std::cout << m_size << std::endl;
     m_original_graph = new zbp::weight*[m_size];
     for (size_t i = 0; i < m_size; i++) {
       m_original_graph[i] = new zbp::weight[m_size];
       std::fill(m_original_graph[i], m_original_graph[i] + m_size, zbp::NO_EDGE);
     }
+
     for (size_t i = 0; i < m_size; i++) {
       size_t left = i > m_neighbourhood_size ? i - m_neighbourhood_size : 0;
       size_t right = i + m_neighbourhood_size < m_size ? i + m_neighbourhood_size : m_size - 1;
--- src/main.cpp
+++ src/main.cpp
@@ -47,102 +47,124 @@ void general_memory_benchmark() {
   run_memory_benchmark(sizes, sizes_length);
 }
 
-//
-//void test(const std::shared_ptr<graph_generator>& graph) {
-//  double duration;
-//
-//  std::cout << "Depth first" << std::endl;
-//  duration = measure_duration(original_depth_first, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_depth_first_sample, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//
-//  std::cout << "Breadth first" << std::endl;
-//  duration = measure_duration(original_breadth_first, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_breadth_first_sample, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//
-//  std::cout << "Floyd-Warshall" << std::endl;
-//  duration = measure_duration(original_floyd_warshall, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_floyd_warshall, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//
-//  std::cout << "Dijkstra" << std::endl;
-//  duration = measure_duration(original_dijkstra, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_dijkstra, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//
-//  std::cout << "Bellman-Ford" << std::endl;
-//  duration = measure_duration(original_bellman_ford, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_bellman_ford, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//
-//  std::cout << "Johnson" << std::endl;
-//  duration = measure_duration(original_johnson, graph);
-//  std::cout << std::endl << "Original: " << duration << std::endl;
-//  duration = measure_duration(boost_johnson, graph);
-//  std::cout << std::endl << "Boost: " << duration << std::endl;
-//}
+void demo(const std::shared_ptr<graph_generator>& graph) {
+  double duration;
+
+  std::cout << "Depth first" << std::endl;
+  duration = OriginalMeasurable(original_depth_first).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostSimpleMeasurable(boost_depth_first).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+
+  std::cout << "Breadth first" << std::endl;
+  duration = OriginalMeasurable(original_breadth_first).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostSimpleMeasurable(boost_breadth_first).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+
+  std::cout << "Floyd-Warshall" << std::endl;
+  duration = OriginalMeasurable(original_floyd_warshall).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostWeightedGraphMeasurable(boost_floyd_warshall).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+
+  std::cout << "Dijkstra" << std::endl;
+  duration = OriginalMeasurable(original_dijkstra).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostWeightedGraphMeasurable(boost_dijkstra).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+
+  std::cout << "Bellman-Ford" << std::endl;
+  duration = OriginalMeasurable(original_bellman_ford).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostWeightedGraphMeasurable(boost_bellman_ford).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+
+  std::cout << "Johnson" << std::endl;
+  duration = OriginalMeasurable(original_johnson).measure_duration(graph);
+  std::cout << std::endl << "Original: " << duration << std::endl;
+  duration = BoostJohnsonGraphMeasurable(boost_johnson).measure_duration(graph);
+  std::cout << std::endl << "Boost: " << duration << std::endl;
+}
 
 void print_sample_graphs() {
-	graph_generator* random_generator;
-	random_generator = new map_graph_generator(20, 100, 10);
-	random_generator->original_graph();
-	random_generator->print();
+  int size = 10;
+  graph_generator* random_generator = new graph_generator(size, 100);
+  random_generator->original_graph();
+  random_generator->print();
+  delete random_generator;
+  std::cout << std::endl;
+
+  random_generator = new graph_generator(size, 50);
+  random_generator->original_graph();
+  random_generator->print();
+  delete random_generator;
+  std::cout << std::endl;
+
+  random_generator = new map_graph_generator(size, 100, 4);
+  random_generator->original_graph();
+  random_generator->print();
+  delete random_generator;
+}
+
+void general_memory_benchmark(char** argv) {
+  graph_generator* random_generator;
+  if (argv[3][0] == 'm') {
+    random_generator = new map_graph_generator(atoi(argv[2]), 100, 10);
+  } else {
+    random_generator = new graph_generator(atoi(argv[2]), atoi(argv[3]));
+  }
+  std::shared_ptr<graph_generator> graph(random_generator);
+  switch (argv[1][0]) {
+  case 'o':
+    std::cout << OriginalMeasurable(original_breadth_first).measure_memory(graph);
+    break;
+  case 's':
+    graph->original_graph();
+    std::cout << BoostSimpleMeasurable(original_breadth_first).measure_memory(graph);
+    break;
+  case 'w':
+    graph->original_graph();
+    std::cout << BoostWeightedGraphMeasurable(original_breadth_first).measure_memory(graph);
+    break;
+  case 'j':
+    graph->original_graph();
+    std::cout << BoostJohnsonGraphMeasurable(original_breadth_first).measure_memory(graph);
+    break;
+  }
+}
+
+void demo_dummy() {
+  graph_generator* generator = new graph_generator();
+  std::shared_ptr<graph_generator> graph(generator);
+  demo(graph);
+}
+
+void demo_filled() {
+  graph_generator* random_generator = new graph_generator(10, 90);
+  std::shared_ptr<graph_generator> random_graph(random_generator);
+  demo(random_graph);
+}
+
+void demo_map() {
+  graph_generator* random_generator = new map_graph_generator(10, 100, 4);
+  std::shared_ptr<graph_generator> random_graph(random_generator);
+  demo(random_graph);
 }
 
 int main(int argc, char **argv) {
   srand(static_cast<unsigned int>(time(NULL)));
 
-  //test();
-
   if (argc == 4) {
-    graph_generator* random_generator;
-    if (argv[3][0] == 'm') {
-      random_generator = NULL; // TODO mapa
-    } else {
-      random_generator = new graph_generator(atoi(argv[2]), atoi(argv[3]));
-    }
-
-    std::shared_ptr<graph_generator> graph(random_generator);
-
-    switch (argv[1][0]) {
-    case 'o':
-      std::cout << OriginalMeasurable(original_breadth_first).measure_memory(graph);
-      break;
-    case 's':
-      graph->original_graph();
-      std::cout << BoostSimpleMeasurable(original_breadth_first).measure_memory(graph);
-      break;
-    case 'w':
-      graph->original_graph();
-      std::cout << BoostWeightedGraphMeasurable(original_breadth_first).measure_memory(graph);
-      break;
-    case 'j':
-      graph->original_graph();
-      std::cout << BoostJohnsonGraphMeasurable(original_breadth_first).measure_memory(graph);
-      break;
-    }
+    general_memory_benchmark(argv);
   } else {
-    print_sample_graphs();
+    demo_map();
+    //print_sample_graphs();
     //general_duration_benchmark();
   }
 
-  /*  graph_generator *generator = new graph_generator();
-   std::shared_ptr<graph_generator> graph(generator);
-   test(graph);
-
-
-   std::cout << "Random" << std::endl;
-   graph_generator *random_generator = new graph_generator(10, 90);
-   std::shared_ptr<graph_generator> random_graph(random_generator);
-   //random_generator->print();
-   test(random_graph);
-   */
+  std::cout << "Random" << std::endl;
+  demo_filled();
 //system("pause");
   return EXIT_SUCCESS;
 }
